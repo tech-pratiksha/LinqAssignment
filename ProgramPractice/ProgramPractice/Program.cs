442c994562ce810028a2b86b800474d6f73a5604
tech-pratiksha <pratikshaprema13579@gmail.com>
2022-06-22 13:00:28 +0530

    Add all my files

---
+++ ProgramPractice/ProgramPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+
+            x = 1000;
+            y = 100;
+            z = x - y;
+
+            Console.WriteLine(z);
+            Console.ReadLine();
+
+        }
+          
+    }
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    clrsccr();
+    int a,b,c;
+    c=a+b;
+    printf("%d",c);
+    getch();
+    
+
